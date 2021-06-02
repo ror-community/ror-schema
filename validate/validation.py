6b320a8c4e2cd1891018fefc166575d837c958b3
Esha Datta <edatta@crossref.org>
2021-06-02 07:28:54 -0400

    added geonames comparison test

--- validate/validation.py
+++ validate/validation.py
@@ -24,22 +24,60 @@ class Validate_Tests:
         # all the validator message use this pattern
         message = {}
         message[name] = {'result':result,'status':True}
-        if msg:
+        if not(result):
             message[name] = {'result':result,'status':msg}
         return message
 
     def __validate_url(self,url):
         msg = None
         validated_url = validators.url(url)
-        if validated_url:
-            try:
-                response = requests.get(url)
-                response.raise_for_status()
-            except requests.exceptions.RequestException as e:
-                msg = "Connection Error"
-        else:
+        if not(validated_url):
             msg = "Validation Error"
         return msg
+
+    def __mapped_geoname_record(self):
+        ror_to_geoname = {
+              "lat": "lat",
+              "lng": "lng",
+              "city": "asciiName",
+              "geonames_city": {
+                "id": "geonameId",
+                "city": "asciiName",
+                "geonames_admin1": {
+                    "name": "adminName1",
+                    "id": "adminId1",
+                    "ascii_name": "adminName1",
+                    "code": ["countryCode","adminCode1"]
+                },
+                "geonames_admin2": {
+                    "name": "adminName2",
+                    "id": "adminId2",
+                    "ascii_name": "adminName2",
+                    "code": ["countryCode","adminCode1","adminCode2"]
+                },
+                "country_geonames_id": "countryId"
+            }}
+        return ror_to_geoname
+
+    def __get_geonames_response(self,id):
+        msg = None
+        result = None
+        query_params = {}
+        query_params['geonameId'] = id
+        query_params['username'] = GEONAMES['USER']
+        url = GEONAMES['URL']
+        try:
+            response = requests.get(url,params=query_params)
+            response.raise_for_status()
+            result = json.loads(response.text)
+        except requests.exceptions.RequestException as e:
+            msg = "Connection Error"
+        return result,msg
+
+    def __get_record_address(self):
+        address = self.__file['addresses'][0]
+        id = address['geonames_city']['id']
+        return id,address
 
     def check_links(self):
         name = str(self.check_links.__name__)
@@ -56,6 +94,47 @@ class Validate_Tests:
         if len(msg) == 0:
             msg = None
         return self.__handle_check(results,name,msg)
+
+    def __compare_ror_geoname(self,mapped_fields,ror_address,geonames_response,msg={}):
+        compare = msg
+        for key, value in mapped_fields.items():
+            # If value is of dict type then print
+            # all key-value pairs in the nested dictionary
+            if isinstance(value, dict):
+                self.__compare_ror_geoname(value,ror_address[key],geonames_response,compare)
+            else:
+                _,original_address = self.__get_record_address()
+                ror_value = ror_address[key] if key in ror_address else original_address[key]
+                geonames_value = None
+                if (key == "code"):
+                    key_exists = True
+                    for x in value:
+                        if not(x in geonames_response):
+                            key_exists = False
+                    if key_exists:
+                        geonames_value = ".".join([geonames_response[x] for x in value])
+                else:
+                    if (value in geonames_response) and (geonames_response[value] != ""):
+                        geonames_value = geonames_response[value]
+                    if str(ror_value) != str(geonames_value):
+                        compare[key] = {"ror": ror_value, "geonames": geonames_value}
+        return compare
+
+    def check_address(self):
+        name = str(self.check_address.__name__)
+        id, address = self.__get_record_address()
+        result = None
+        compare = {}
+        geonames_response,msg = self.__get_geonames_response(id)
+        if geonames_response:
+            mapped_fields = self.__mapped_geoname_record()
+            compare = self.__compare_ror_geoname(mapped_fields,address,geonames_response)
+            if len(compare) == 0:
+                    result = True
+        else:
+            compare["ERROR"] = msg
+        print(compare)
+        return self.__handle_check(result,name,compare)
 
 
     def check_country_code(self):
@@ -106,6 +185,6 @@ print(validate.validate_all())
 #TODO:
 # have getter functions for all the validation functions -- ??
 # finish relationship checking
-# implement geonames,
+# implement geonames - done,
 # url checking - done
 # write tests
